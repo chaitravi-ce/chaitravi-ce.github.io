77d63897515430061f21c11e756a4162705a7cee
chaitravi-ce <chaitravi007@gmail.com>
2022-02-08 11:37:41 +0530

    Added Responsiveness

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -14,48 +14,50 @@ const Experience = () => {
     return(
         <div className="exp">
             <h1>Experience</h1>
-            <Grid container spacing={4}>
-                <Grid item xs={3}>
-                    <Cards 
-                        name="IIT Bombay" 
-                        position="Research Intern" 
-                        description="Analyzed Transfer Learning on Vibrational Dataset for Supervised and Unsupervised Learning using a 
-                        memory-aware technique. Implemented Edge deployment of the models."
-                        logo={iit}
-                        img={iitBack}
-                    />
+            <div className='grid'>
+                <Grid container spacing={4} >
+                    <Grid item xs={12} md={3} sm={6}>
+                        <Cards 
+                            name="IIT Bombay" 
+                            position="Research Intern" 
+                            description="Analyzed Transfer Learning on Vibrational Dataset for Supervised and Unsupervised Learning using a 
+                            memory-aware technique. Implemented Edge deployment of the models."
+                            logo={iit}
+                            img={iitBack}
+                        />
+                    </Grid>
+                    <Grid item xs={12} md={3} sm={6}>
+                        <Cards 
+                            name="Vineyard-CNCF" 
+                            position="Open Source Developer" 
+                            description="Contributed to open source through Open Source Promotion Plan (OSPP) by creating a connection between 
+                            Vineyard objects and Machine Learning Frameworks such as TensorFlow, PyTorch, MxNet and Nvidia-DALI"
+                            logo={vin}
+                            img={osppBack}
+                        />
+                    </Grid>
+                    <Grid item xs={12} md={3} sm={6}>
+                        <Cards 
+                            name="AIRPIX" 
+                            position="AI/ML Intern" 
+                            description="Worked on Detection of various road assets using Video Analytics, AI and Image Processing. Mainly focused 
+                            on developing algorithms based on Semantic Segmentation and Object Detection."
+                            logo={airpix}
+                            img={airpixBack}
+                        />
+                    </Grid>
+                    <Grid item xs={12} md={3} sm={6}>
+                        <Cards 
+                            name="Magnes Motors" 
+                            position="App Development Intern" 
+                            description="Developed a Flutter based cross-platform application for EV station which included QR scanning and 
+                            communicating to ESP based hardware"
+                            logo={magnes}
+                            img={magnesBack}
+                        />
+                    </Grid>
                 </Grid>
-                <Grid item xs={3}>
-                    <Cards 
-                        name="Vineyard-CNCF" 
-                        position="Open Source Developer" 
-                        description="Contributed to open source through Open Source Promotion Plan (OSPP) by creating a connection between 
-                        Vineyard objects and Machine Learning Frameworks such as TensorFlow, PyTorch, MxNet and Nvidia-DALI"
-                        logo={vin}
-                        img={osppBack}
-                    />
-                </Grid>
-                <Grid item xs={3}>
-                    <Cards 
-                        name="AIRPIX" 
-                        position="AI/ML Intern" 
-                        description="Worked on Detection of various road assets using Video Analytics, AI and Image Processing. Mainly focused 
-                        on developing algorithms based on Semantic Segmentation and Object Detection."
-                        logo={airpix}
-                        img={airpixBack}
-                    />
-                </Grid>
-                <Grid item xs={3}>
-                    <Cards 
-                        name="Magnes Motors" 
-                        position="App Development Intern" 
-                        description="Developed a Flutter based cross-platform application for EV station which included QR scanning and 
-                        communicating to ESP based hardware"
-                        logo={magnes}
-                        img={magnesBack}
-                    />
-                </Grid>
-            </Grid>
+            </div>
         </div>
     );
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,18 +6,20 @@ const HeroCommon = () => {
   return (
     <div className="hero">
       <Navbar />
-      <div className="socials column">
-        <a href="mailto:cnchalke_b19@el.vjti.ac.in"><i class="fa fa-inbox"></i></a>
-        <a href="https://www.linkedin.com/in/chaitravi-chalke-6b4806190/"><i class="fa fa-linkedin"></i></a>
-        <a href="https://github.com/chaitravi-ce"><i class="fa fa-github-alt"></i></a>
-        <a href="https://twitter.com/Chaitravi_ce"><i class="fa fa-twitter"></i></a>
-        <a href="https://www.instagram.com/chaitravi_ce25/"><i class="fa fa-instagram"></i></a>
-      </div>
-      <div className="main-text column">
-        <h1>Hi, I'm Chaitravi Chalke</h1>
-        <p>I am a polymath who likes to learn new things in my free time. 
-          I have tried my hands on AI/ML, Application Development and Web Development in the process of 
-          developing cool some cool projects.</p>
+      <div className="landing">
+        <div className="socials">
+          <a href="mailto:cnchalke_b19@el.vjti.ac.in"><i class="fa fa-inbox"></i></a>
+          <a href="https://www.linkedin.com/in/chaitravi-chalke-6b4806190/"><i class="fa fa-linkedin"></i></a>
+          <a href="https://github.com/chaitravi-ce"><i class="fa fa-github-alt"></i></a>
+          <a href="https://twitter.com/Chaitravi_ce"><i class="fa fa-twitter"></i></a>
+          <a href="https://www.instagram.com/chaitravi_ce25/"><i class="fa fa-instagram"></i></a>
+        </div>
+        <div className="main-text">
+          <h1>Hi, I'm Chaitravi Chalke</h1>
+          <p>I am a polymath who likes to learn new things in my free time. 
+            I have tried my hands on AI/ML, Application Development and Web Development in the process of 
+            developing cool some cool projects.</p>
+        </div>
       </div>
     </div>
   );
