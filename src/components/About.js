09d100e699dc18e36140131579bd39008e272c46
chaitravi-ce <chaitravi007@gmail.com>
2022-02-11 14:21:08 +0530

    Added Skills Section

--- src/components/About.js
+++ src/components/About.js
@@ -18,13 +18,22 @@ const About = () => {
             <div className='skills'>
                 <h3>Skills</h3>
                 <div className='sicons'>
-                    <Skill class="fa fa-html5 fd" title="HTML5"/> 
-                    <Skill class="fa fa-css3 fd" title="CSS3"/>
-                    <Skill class="fa fa-github fd" title="GITHUB"/>
-                    <Skill class="fa fa-twitter fd" title="DOCKER"/>
-                    <Skill class="fa fa-linkedin fd" title="DOCKER"/>
-                    <Skill class="fa fa-linkedin fd" title="DOCKER"/>
-                    <Skill class="fa fa-linkedin fd" title="DOCKER"/>
+                    <Skill class="fa-solid fa-chart-pie fd" title="Data Science" />
+                    <Skill class="fa-solid fa-brain fd" title="Deep Learning" />
+                    <Skill class="fa-brands fa-android fd" title="Flutter" />
+                    <Skill class="fa-solid fa-fire-flame-curved fd" title="Firebase"/>
+                    <Skill class="fa-brands fa-html5 fd" title="HTML5" />
+                    <Skill class="fa-brands fa-css3 fd" title="CSS3" />
+                    <Skill class="fa-brands fa-sass fd" title="SASS" />
+                    <Skill class="fa-brands fa-js-square fd" title="JavaScript" />
+                    <Skill class="fa-brands fa-react fd" title="React" />
+                    <Skill class="fa-solid fa-database fd" title="SQL Database" />
+                    <Skill class="fa-solid fa-leaf fd" title="MongoDB" />
+                    <Skill class="fa-brands fa-python fd" title="Python" />
+                    <Skill class="fa-solid fa-c fd" title="C-C++" />
+                    <Skill class="fa-brands fa-github fd" title="Git-Github" />
+                    <Skill class="fa-brands fa-docker fd" title="Docker" />
+                    {/* <Skill class="fa-solid fa-clapperboard fd" title="Video Editing" /> */}
                 </div>
             </div>
         </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -10,7 +10,7 @@ const Cards = (props) => {
                         <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
                         <div className="card__thumb" style={{backgroundImage: `url(${props.logo})`}}/>
                         <div className="card__header-text">
-                            <h3 className="card__title">{props.name}</h3>            
+                            <h6 className="card__title">{props.name}</h6>            
                             <span className="card__status">{props.position}</span>
                         </div>
                     </div>
